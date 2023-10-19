4e3d1618481fe302a6134bc57fb8e8fa6e9ebd80
Mariappan Sivakumar <mariappan.sivakumar@zucisystems.com>
2023-10-19 21:35:11 +0530

    first commit

---
+++ src/test/java/com/app/shopping/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.shopping.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
